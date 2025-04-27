10f85b4221e0dbaa22c76f5333c675e7e62d0573
Paper02 <Holacasa898@gmail.com>
2025-04-27 15:03:11 -0400

    Adds main And part of the model

---
+++ Model/board.h
@@ -0,0 +1,8 @@
+#ifndef C_BOARD
+	#define C_BOARD
+	typedef struct Board {
+		char **grid; // 2D array of chars. grid is a pointer to a pointer. See boardExample for how the grid looks like.
+
+	};
+
+#endif // !C_BOARD
---
+++ main.c
@@ -0,0 +1,8 @@
+
+// This will be the file that coordinates the mvc architecture
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
