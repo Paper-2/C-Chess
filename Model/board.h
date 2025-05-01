322d93556db1f4ff10627a97a8ba8aa8f7ba52fa
Paper02 <Holacasa898@gmail.com>
2025-04-30 21:44:34 -0400

    Updates the folder structure More logs

--- Model/board.h
+++
@@ -1,8 +0,0 @@
-#ifndef C_BOARD
-	#define C_BOARD
-	typedef struct Board {
-		char **grid; // 2D array of chars. grid is a pointer to a pointer. See boardExample for how the grid looks like.
-
-	};
-
-#endif // !C_BOARD
