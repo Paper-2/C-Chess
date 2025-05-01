e517459750609070c5a5c65509a30a44f7f6044f
Paper02 <Holacasa898@gmail.com>
2025-04-30 21:47:26 -0400

    Add piece management functions and definitions

---
+++ src/piece.c
@@ -0,0 +1,66 @@
+
+
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "piece.h"
+
+const char *colors[] = {"White", "Black"};
+const char *classes[] = {"King", "Queen", "Rook", "Bishop", "Knight", "Pawn"};
+
+inline void setPieceBitMask(Piece *bitmask, uint8_t pieceBitmask)
+{
+	// changes the bitmask of a piece to a given value
+	*bitmask = pieceBitmask;
+}
+inline void setColor(Piece *bitmask, uint8_t color)
+{
+	// sets the color
+	*bitmask = (*bitmask & 0b11110) | color; // bits 1 to 3 will be kept the same, bit 0 will not.
+}
+inline void setClass(Piece *bitmask, uint8_t pieceType)
+{
+	// sets the class
+	*bitmask = (*bitmask & 0b0001) | (pieceType << 1); // bit 0 will be kept the same, bits 1 to 3 will be changed.
+}
+
+void printPiece(Piece *bitmask)
+{
+	// prints the piece
+
+	uint8_t color = getColor(bitmask);
+	uint8_t classType = getClass(bitmask) >> 1;
+
+	printf("Piece: %d\n", *bitmask);
+	printf("Color: %s\n", colors[color]);
+	printf("Class: %s\n", classes[classType]);
+}
+
+inline void killPiece(Piece *bitmask)
+{
+	// kills a piece by deleting it
+	free(bitmask);
+}
+
+inline int getColor(Piece *bitmask)
+{
+	// gets the color of a piece
+	return *bitmask & 0b0001;
+}
+inline int getClass(Piece *bitmask)
+{
+	// gets the type of a piece
+	return *bitmask & 0b1110;
+}
+
+int main()
+{
+	Piece *piece = malloc(sizeof(Piece));
+	setPieceBitMask(piece, 0b0000);
+	setColor(piece, 1);
+	setClass(piece, 2);
+	printPiece(piece);
+	getColor(piece);
+	killPiece(piece);
+	return 0;
+}
---
+++ src/piece.h
@@ -0,0 +1,25 @@
+
+
+#ifndef PIECE_H
+#define PIECE_H
+
+// Piece is an integer. It is used to represent a piece on the board. 
+// The value of the piece is the index of the piece in the pieces array. 
+// The pieces array is a 2D array of chars. The first dimension is the number of pieces.
+// The second dimension is the size of each piece. The size of each piece is the number of squares in the piece.
+// The number of squares in the piece is the number of squares in the grid. The grid is a pointer to a pointer.
+// See boardExample for how the grid looks like.
+#include <stdint.h>
+typedef uint8_t Piece;
+
+
+void	setPieceBitMask(Piece*, uint8_t);	// changes the bitmask of a piece to a given value
+void	setColor(Piece*, uint8_t);			// sets the color
+void	setClass(Piece*, uint8_t);			// sets the class
+
+void	killPiece(Piece*);					// kills a piece by deleting it and deallocating the memory
+
+int		getColor(Piece *);					// gets the color of a piece
+int		getClass(Piece *);					// gets the type of a piece
+
+#endif // !Piece
