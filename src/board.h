7fa36853295c78bd85a9da7c12f9624312a17495
Paper02 <Holacasa898@gmail.com>
2025-04-30 21:50:04 -0400

    Adds initial Board struct definition

---
+++ src/board.h
@@ -0,0 +1,8 @@
+#ifndef BOARD_H
+#define BOARD_H
+typedef struct Board {
+	char **grid; // 2D array of chars. grid is a pointer to a pointer. See boardExample for how the grid looks like.
+
+};
+
+#endif // !BOARD_H
