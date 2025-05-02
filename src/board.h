80d42455b90039d266b22c71969087053d8424ac
Paper02 <Holacasa898@gmail.com>
2025-05-02 19:20:08 -0400

    Adds the starting code for movement validation - changed isValidMove to have a src parameter

--- src/board.h
+++ src/board.h
@@ -15,7 +15,15 @@ void printBoard(Board *);
 void movePiece(Board *, Piece *, int[2]); 			// moves a piece from one position to another. sets the destination cell to the piece and the source cell to an empty cell.
 void setBoard(Board *, Piece[8][8]);		  		// sets the board based on the fen string
 
-int isvalidMove(Board *, Piece *, int[2], int[2]); 	// checks if the move is valid (for all pieces types)
+int isvalidMove(Board *, Piece *, int[2],  int[2]); 	// checks if the move is valid (for all pieces types)
+
+int isvalidMoveKing(Board *, Piece *, int[2],  int[2]);	// checks if the move is valid for a king
+int isvalidMoveQueen(Board *, Piece *, int[2],  int[2]);	// checks if the move is valid for a queen
+int isvalidMoveRook(Board *, Piece *, int[2],  int[2]);	// checks if the move is valid for a rook
+int isvalidMoveBishop(Board *, Piece *, int[2],  int[2]);	// checks if the move is valid for a bishop
+int isvalidMoveKnight(Board *, Piece *, int[2],  int[2]);	// checks if the move is valid for a knight
+int isvalidMovePawn(Board *, Piece *, int[2],  int[2]);	// checks if the move is valid for a pawn
+
 int isSpaceFree(Board *, int[2]);					// checks if the space is empty
 
 #endif // !BOARD_H
